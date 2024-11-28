dd688a50212e75144f411034a17d682f8b4e9a9b
Lenny Malard <lennymalard@gmail.com>
2024-11-28 18:49:38 +0100

    Added Adam optimizer.

--- melpy/optimizers.py
+++ melpy/optimizers.py
@@ -1,4 +1,5 @@
 from .functions import *
+from math import sqrt
 
 class Optimizer:
     """
@@ -65,12 +66,12 @@ class SGD(Optimizer):
         """
         if isinstance(layer, Dense) or isinstance(layer, Convolution2D):
             if self.momentum is not None:
-                weights = self.momentum * layer.w_momentum - layer.dW * self.learning_rate
+                weights = self.momentum * layer.weight_momentums - layer.dW * self.learning_rate
                 if layer.biases is not None:
-                    biases = self.momentum * layer.b_momentum - layer.dB * self.learning_rate
+                    biases = self.momentum * layer.bias_momentums - layer.dB * self.learning_rate
 
-                layer.w_momentum = weights
-                layer.b_momentum = biases
+                layer.weight_momentums = weights
+                layer.bias_momentums = biases
                 layer.weights += weights
                 layer.biases += biases
 
@@ -82,20 +83,69 @@ class SGD(Optimizer):
         return layer
 
 class Adam(Optimizer):
+    """
+    Adam optimizer.
+
+    Attributes
+    ----------
+    beta1 : float
+        The exponential decay rate for the first moment estimates.
+    beta2 : float
+        The exponential decay rate for the second moment estimates.
+    epsilon : float
+        A small constant for numerical stability.
+    step : int
+        The current step of the optimizer.
+
+    Methods
+    -------
+    update_params(layer : melpy.Layer)
+        Updates the parameters of the given layer using Adam.
+    """
     def __init__(self):
+        """
+        Initializes the Adam optimizer.
+        """
         super().__init__()
         self.beta1 = 0.9
         self.beta2 = 0.999
         self.epsilon = 1e-7
+        self.step = 0
 
     def update_params(self, layer):
-        if isinstance(layer, Dense) or isinstance(layer, Convolution2D):
-            weights = self.momentum * layer.w_momentum - layer.dW * self.learning_rate
-            if layer.biases is not None:
-                biases = self.momentum * layer.b_momentum - layer.dB * self.learning_rate
+        """
+        Updates the parameters of the given layer using Adam.
 
-            layer.w_momentum = weights
-            layer.b_momentum = biases
+        Parameters
+        ----------
+        layer : melpy.Layer
+            The layer whose parameters are to be updated.
+
+        Returns
+        -------
+        layer : melpy.Layer
+            The layer with updated parameters.
+        """
+        if isinstance(layer, Dense) or isinstance(layer, Convolution2D):
+            layer.weight_momentums = self.beta1 * layer.weight_momentums + (1 - self.beta1) * layer.dW
+            layer.bias_momentums = self.beta1 * layer.bias_momentums + (1 - self.beta1) * layer.dB
+
+            weight_momentums_corrected = layer.weight_momentums / (1 - self.beta1**self.step)
+            bias_momentums_corrected = layer.bias_momentums / (1 - self.beta1**self.step)
+
+            layer.weight_cache = self.beta2 * layer.weight_cache + (1 - self.beta2) * layer.dW**2
+            layer.bias_cache = self.beta2 * layer.bias_cache + (1 - self.beta2) * layer.dB**2
+
+            weight_cache_corrected = layer.weight_cache / (1 - self.beta2**self.step)
+            bias_cache_corrected = layer.bias_cache / (1 - self.beta2**self.step)
+
+            weights = - self.learning_rate * weight_momentums_corrected / (sqrt(weight_cache_corrected) + self.epsilon)
+            if layer.biases is not None:
+                biases = - self.learning_rate * bias_momentums_corrected / (sqrt(bias_cache_corrected) + self.epsilon)
+
             layer.weights += weights
             layer.biases += biases
 
+            self.step += 1
+
+        return layer
