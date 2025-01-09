3877da26641f41f0bf53b3d02c107c30d9e2439f
Lenny Malard <lennymalard@gmail.com>
2025-01-09 23:17:36 +0100

    Corrected metrics.

--- melpy/metrics.py
+++ melpy/metrics.py
@@ -22,20 +22,24 @@ def accuracy(targets, predictions):
     ValueError
         If the shapes of `targets` and `outputs` are incompatible.
     """
-    if isinstance(targets, np.ndarray):
-        targets = Tensor(targets)
+    if isinstance(targets, Tensor) or isinstance(targets, Operation):
+        targets = targets.array
 
-    if isinstance(predictions, np.ndarray):
-        predictions = Tensor(predictions)
+    if isinstance(predictions, Tensor) or isinstance(predictions, Operation):
+        predictions = predictions.array
 
-    predicted_labels = np.argmax(predictions.array, axis=1)
-    true_labels = np.argmax(targets.array, axis=1)
+    if targets.shape[1] != 1 and predictions.shape[1] != 1:
+        predicted_labels = np.argmax(predictions, axis=1)
+        true_labels = np.argmax(targets, axis=1)
+    else:
+        predicted_labels = np.around(predictions)
+        true_labels = np.around(targets)
 
     return  np.sum(predicted_labels == true_labels) / len(true_labels)
 
-def differences(targets, predictions):
+def mean_absolute_error(targets, predictions):
     """
-    Prints the differences between the target values and the predicted output values.
+    Prints the mean absolute error between the target values and the predicted output values.
 
     Parameters
     ----------
@@ -49,14 +53,10 @@ def differences(targets, predictions):
     ValueError
         If the lengths of `targets` and `outputs` are not the same.
     """
-    if isinstance(targets, np.ndarray):
-        targets = Tensor(targets)
+    if isinstance(targets, Tensor) or isinstance(targets, Operation):
+        targets = targets.array
 
-    if isinstance(predictions, np.ndarray):
-        predictions = Tensor(predictions)
+    if isinstance(predictions, Tensor) or isinstance(predictions, Operation):
+        predictions = predictions.array
 
-    for i in range(len(predictions.array)):
-        if predictions.array[i] >= targets.array[i]:
-            print(predictions.array[i] - targets.array[i])
-        if predictions.array[i] < targets.array[i]:
-            print(targets.array[i] - predictions.array[i])
+    return np.abs(targets - predictions).mean()
