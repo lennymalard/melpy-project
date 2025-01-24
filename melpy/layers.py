28c881a82edf6504e4dd1d627acb4eb257169df3
Lenny Malard <lennymalard@gmail.com>
2025-01-24 20:00:22 +0100

    Fused LSTM parameters and changed how parameters are handled in layers.

--- melpy/layers.py
+++ melpy/layers.py
@@ -214,8 +214,8 @@ class Dense(Layer):
                     "`weight_init` must be either 'he_uniform', 'he_normal', 'glorot_uniform' or 'glorot_normal'.")
             return weights
 
-        self.weights = initialize_weights(weight_initializer, in_features, out_features)
-        self.biases = Parameter(np.random.rand(1, out_features), requires_grad=True)
+        self.parameters = [initialize_weights(weight_initializer, in_features, out_features), Parameter(np.random.rand(1, out_features), requires_grad=True)]
+
         self.dW = np.zeros_like(self.weights.array)
         self.dB = np.zeros_like(self.biases.array)
 
@@ -226,6 +226,22 @@ class Dense(Layer):
         self.flattened_input = False
 
         self.activation = initialize_activation(activation, Layer)
+
+    @property
+    def weights(self):
+        return self.parameters[0]
+
+    @weights.setter
+    def weights(self, value):
+        self.parameters[0] = value
+
+    @property
+    def biases(self):
+        return self.parameters[1]
+
+    @biases.setter
+    def biases(self, value):
+        self.parameters[1] = value
 
     def forward(self):
         """
@@ -806,21 +822,38 @@ class Convolution2D(Layer):
 
         self.in_channels = in_channels
         self.out_channels = out_channels
-        self.weights = initialize_weights(weight_initializer, in_channels, out_channels, kernel_size)
+        self.parameters = [initialize_weights(weight_initializer, in_channels, out_channels, kernel_size), None]
         self.dW = np.zeros_like(self.weights)
+
         if use_bias is True:
-            self.biases = Parameter(np.zeros(shape=(1, out_channels, 1, 1)).astype(np.float64), requires_grad=True)
+            self.parameters[1] = Parameter(np.zeros(shape=(1, out_channels, 1, 1)).astype(np.float64), requires_grad=True)
             self.dB = np.zeros_like(self.biases)
-        else:
-            self.biases = None
+
         self.padding = padding
         self.kernel_size = kernel_size
         self.stride = stride
+        self.use_bias = use_bias
 
         self.activation = initialize_activation(activation, Layer)
 
         if self.padding == "same":
             self.stride = 1
+
+    @property
+    def weights(self):
+        return self.parameters[0]
+
+    @weights.setter
+    def weights(self, value):
+        self.parameters[0] = value
+
+    @property
+    def biases(self):
+        return self.parameters[1]
+
+    @biases.setter
+    def biases(self, value):
+        self.parameters[1] = value
 
     def calculate_padding(self):
         """
@@ -916,8 +949,8 @@ class Convolution2D(Layer):
             (self.input_padded.shape[0], self.out_channels, output_height, output_width)
         ), requires_grad=True)
 
-        if self.biases is not None:
-            self.outputs = self.outputs + self.biases
+        if self.use_bias:
+            self.outputs += self.biases
 
         if self.activation is not None:
             self.activation.inputs = self.outputs
@@ -1335,16 +1368,42 @@ class LSTMCell(Layer):
         self.sequence_inputs = []
         self.sequence_inputs_grads = []
 
-        self.input_weights = self.Weights((input_size, hidden_size))
-        self.hidden_weights = self.Weights((hidden_size, hidden_size))
-        self.biases = self.Biases((1, hidden_size)) if use_bias else None
+        self.parameters = [
+            self.Weights((input_size, hidden_size)),
+            self.Weights((hidden_size, hidden_size))
+        ]
 
-        self.i_input_weights, self.f_input_weights, self.c_input_weights, self.o_input_weights = self.input_weights.split()
-        self.i_hidden_weights, self.f_hidden_weights, self.c_hidden_weights, self.o_hidden_weights = self.hidden_weights.split()
-        self.i_biases, self.f_biases, self.c_biases, self.o_biases = self.biases.split()
+        if self.use_bias:
+            self.parameters.append(self.Biases((1, hidden_size)))
 
         self.dC = None
         self.dH = None
+
+    @property
+    def input_weights(self):
+        return self.parameters[0]
+
+    @input_weights.setter
+    def input_weights(self, value):
+        self.parameters[0] = value
+
+    @property
+    def hidden_weights(self):
+        return self.parameters[1]
+
+    @hidden_weights.setter
+    def hidden_weights(self, value):
+        self.parameters[1] = value
+
+    @property
+    def biases(self):
+        if self.use_bias:
+            return self.parameters[2]
+
+    @biases.setter
+    def biases(self, value):
+        if self.use_bias:
+            self.parameters[2] = value
 
     def forward(self, inputs, hidden_state, cell_state):
         """
@@ -1372,21 +1431,17 @@ class LSTMCell(Layer):
         self.previous_hidden_state = Tensor(hidden_state.array)
         self.previous_cell_state = Tensor(cell_state.array)
 
-        input_gate = self.inputs @ self.i_input_weights + self.previous_hidden_state @ self.i_hidden_weights
-        output_gate = self.inputs @ self.o_input_weights + self.previous_hidden_state @ self.o_hidden_weights
-        forget_gate = self.inputs @ self.f_input_weights + self.previous_hidden_state @ self.f_hidden_weights
-        cell_input = self.inputs @ self.c_input_weights + self.previous_hidden_state @ self.c_hidden_weights
+        self.gates = self.inputs @ self.input_weights + self.previous_hidden_state @ self.hidden_weights
 
         if self.use_bias:
-            input_gate += self.i_biases
-            output_gate += self.o_biases
-            forget_gate += self.f_biases
-            cell_input += self.c_biases
+            self.gates += self.biases
 
-        self.input_gate = sigmoid(input_gate)
-        self.output_gate = sigmoid(output_gate)
-        self.forget_gate = sigmoid(forget_gate)
-        self.cell_input = self.activation(cell_input)
+        input_gate, forget_gate, cell_input, output_gate = np.hsplit(self.gates.array, 4)
+
+        self.input_gate = sigmoid(Tensor(input_gate))
+        self.forget_gate = sigmoid(Tensor(forget_gate))
+        self.cell_input = self.activation(Tensor(cell_input))
+        self.output_gate = sigmoid(Tensor(output_gate))
 
         self.cell_state = self.forget_gate * self.previous_cell_state + self.input_gate * self.cell_input
         self.hidden_state = self.output_gate * self.activation(self.cell_state)
@@ -1422,6 +1477,11 @@ class LSTMCell(Layer):
         dX : ndarray
             Gradient of loss with respect to the input.
         """
+
+        self.i_input_weights, self.f_input_weights, self.c_input_weights, self.o_input_weights = self.input_weights.split()
+        self.i_hidden_weights, self.f_hidden_weights, self.c_hidden_weights, self.o_hidden_weights = self.hidden_weights.split()
+        self.i_biases, self.f_biases, self.c_biases, self.o_biases = self.biases.split()
+
         self.dC = (Tensor(hidden_state_grad) * self.output_gate * Tensor(self.activation(self.cell_state).derivative()) +
                    Tensor(cell_state_grad) * self.next_forget_gate).array
 
@@ -1435,21 +1495,19 @@ class LSTMCell(Layer):
         o_dZ = dO * self.output_gate.array * (1 - self.output_gate.array)
         c_dZ = dTildeC * self.cell_input.derivative()
 
-        self.i_input_weights.grad += self.inputs.T.array @ i_dZ
-        self.f_input_weights.grad += self.inputs.T.array @ f_dZ
-        self.o_input_weights.grad += self.inputs.T.array @ o_dZ
-        self.c_input_weights.grad += self.inputs.T.array @ c_dZ
+        gates_grad = np.hstack(
+            [i_dZ,
+             f_dZ,
+             c_dZ,
+             o_dZ
+             ]
+        )
 
-        self.i_hidden_weights.grad += self.previous_hidden_state.T.array @ i_dZ
-        self.f_hidden_weights.grad += self.previous_hidden_state.T.array @ f_dZ
-        self.o_hidden_weights.grad += self.previous_hidden_state.T.array @ o_dZ
-        self.c_hidden_weights.grad += self.previous_hidden_state.T.array @ c_dZ
+        self.input_weights.grad += self.inputs.T.array @ gates_grad
+        self.hidden_weights.grad += self.previous_hidden_state.T.array @ gates_grad
 
         if self.use_bias:
-            self.i_biases.grad += np.sum(i_dZ, axis=0, keepdims=True)
-            self.f_biases.grad += np.sum(f_dZ, axis=0, keepdims=True)
-            self.o_biases.grad += np.sum(o_dZ, axis=0, keepdims=True)
-            self.c_biases.grad += np.sum(c_dZ, axis=0, keepdims=True)
+            self.biases.grad += np.sum(gates_grad, axis=0, keepdims=True)
 
         self.dH = i_dZ @ self.i_hidden_weights.T.array + f_dZ @ self.f_hidden_weights.T.array + \
                   o_dZ @ self.o_hidden_weights.T.array + c_dZ @ self.c_hidden_weights.T.array
@@ -1481,20 +1539,11 @@ class LSTMCell(Layer):
         -------
         None
         """
-        self.i_input_weights.zero_grad()
-        self.f_input_weights.zero_grad()
-        self.c_input_weights.zero_grad()
-        self.o_input_weights.zero_grad()
+        self.input_weights.zero_grad()
+        self.hidden_weights.zero_grad()
 
-        self.i_hidden_weights.zero_grad()
-        self.f_hidden_weights.zero_grad()
-        self.c_hidden_weights.zero_grad()
-        self.o_hidden_weights.zero_grad()
-
-        self.i_biases.zero_grad()
-        self.f_biases.zero_grad()
-        self.o_biases.zero_grad()
-        self.c_biases.zero_grad()
+        if self.use_bias:
+            self.biases.zero_grad()
 
         self.sequence_inputs_grads = []
 
@@ -1766,12 +1815,19 @@ class Embedding(Layer):
 
         self.inputs = None
         self.outputs = None
-        self.weights = initialize_weights(weight_initializer, input_dim, output_dim)
-        self.biases = None
+        self.parameters = [initialize_weights(weight_initializer, input_dim, output_dim)]
         self.dX = None
         self.dW = None
 
         self.flattened_input = False
+
+    @property
+    def weights(self):
+        return self.parameters[0]
+
+    @weights.setter
+    def weights(self, value):
+        self.parameters[0] = value
 
     def forward(self):
         check_tensor(self.inputs, "inputs")
